c4233b6dc7196e6e9533b9c5b74a1e312182d2ba
Matt Hughes <matt.hughes@calgaryscientific.com>
2015-08-15 13:22:07 -0600

    A basic model and view.

--- Main.elm
+++ Main.elm
@@ -1,3 +1,32 @@
 import Graphics.Element exposing (..)
 
-main = show "Hello world"
+main = view
+
+type Move = Empty
+          | X
+          | O
+
+model = [[X, O, X], 
+         [Empty, X, O],
+         [X, O, Empty]]
+
+
+view = 
+  (flow down (List.map viewRow model))
+
+viewRow r = 
+  flow right (List.map viewMove r)
+
+viewMove : Move -> Element
+viewMove m = 
+  case m of
+    Empty -> 
+      show " "
+
+
+    X ->
+      show "X"
+
+
+    O -> 
+      show "O"
