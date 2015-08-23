4b9e44fd72276561920d8d58b42810b4a300b7a1
Matt Hughes <matt.hughes@calgaryscientific.com>
2015-08-23 08:32:31 -0600

    Giving main it's own model

--- Main.elm
+++ Main.elm
@@ -7,7 +7,7 @@ main : Signal Element
 main = 
   Signal.map2 view Window.dimensions currentState
 
-currentState : Signal Tictactoe.Model
+currentState : Signal Model
 currentState = Signal.foldp
                (\(Just action) model -> update action model)
                init
@@ -19,13 +19,14 @@ boardClick = Signal.mailbox Nothing
 address = Signal.forwardTo boardClick.address Just
 
 -- Model
-type alias Model = Tictactoe.Model
-init : Tictactoe.Model
-init = Tictactoe.init
+type alias Model = { t3m : Tictactoe.Model }
+init : Model
+init = { t3m = Tictactoe.init }
 
 -- Update
 update : Tictactoe.Action -> Model -> Model
-update = Tictactoe.update
+update a m = { m | t3m <- Tictactoe.update a m.t3m }
 
 -- View
-view = Tictactoe.view address
+view : (Int, Int) -> Model -> Element
+view w m = Tictactoe.view address w m.t3m
