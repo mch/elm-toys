3b5c98943c815ac362fa36d8a94c1967b1db0e55
Matt Hughes <matt.hughes@calgaryscientific.com>
2015-09-03 19:09:15 -0600

    Adding a rudimentary menu

--- Main.elm
+++ Main.elm
@@ -3,7 +3,8 @@ import Window
 import Tictactoe
 import Snake
 import Graphics.Element exposing (..)
-
+import Graphics.Input exposing (clickable)
+import Text exposing (fromString)
 
 -- Infrastructure, see also StartApp
 main : Signal Element
@@ -23,24 +24,27 @@ address = Signal.forwardTo mailbox.address Just
 
 
 -- Main App itself
-type Action = TictactoeMove Tictactoe.Action
+type Action = MenuSelection Toy
+            | TictactoeMove Tictactoe.Action
             | SnakeMove Snake.Input
 
 t3Address = Signal.forwardTo address TictactoeMove
 snakeAddress = Signal.forwardTo address SnakeMove
+menuAddress = Signal.forwardTo address MenuSelection
 
 -- Model
-type Game = TictactoeGame | SnakeGame
+type Toy = Menu | TictactoeGame | SnakeGame
 
-type alias Model = { currentGame: Game, t3m : Tictactoe.Model, s : Snake.Model }
+type alias Model = { currentToy: Toy, t3m : Tictactoe.Model, s : Snake.Model }
 init : Model
-init = { currentGame = TictactoeGame, t3m = Tictactoe.init, s = Snake.init }
+init = { currentToy = Menu, t3m = Tictactoe.init, s = Snake.init }
 
 
 -- Update
 update : Action -> Model -> Model
 update a m =
   case a of
+    MenuSelection t -> { m | currentToy <- t }
     TictactoeMove ta -> { m | t3m <- Tictactoe.update ta m.t3m }
     SnakeMove i -> { m | s <- Snake.update i m.s }
 
@@ -48,7 +52,26 @@ update a m =
 -- View
 view : (Int, Int) -> Model -> Element
 view w m = 
-  case m.currentGame of
-    TictactoeGame -> Tictactoe.view t3Address w m.t3m
-    SnakeGame -> Snake.view snakeAddress m.s
+  case m.currentToy of
+    Menu -> viewMenu menuAddress w m
+    TictactoeGame -> (Tictactoe.view t3Address w m.t3m) `below` (viewMainMenuButton menuAddress)
+    SnakeGame -> (Snake.view snakeAddress m.s) `below` (viewMainMenuButton menuAddress)
 
+
+viewMainMenuButton : (Signal.Address Toy) -> Element
+viewMainMenuButton a =
+  fromString "Menu Menu"
+    |> centered
+    |> clickable (Signal.message a Menu)
+
+
+viewMenu : (Signal.Address Toy) -> (Int, Int) -> Model -> Element
+viewMenu a (wx, wy) m =
+  container wx wy middle (flow down [viewMenuItem a "Tictactoe" TictactoeGame,
+                                     viewMenuItem a "Snake" SnakeGame])
+
+viewMenuItem : Signal.Address Toy -> String -> Toy -> Element
+viewMenuItem a s t =
+  fromString s
+    |> centered
+    |> clickable (Signal.message a t)
