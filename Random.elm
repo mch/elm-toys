ddc983d1fc4eb6da2bd4e78cf9db45b9906c52d2
Matt Hughes <matt.hughes@calgaryscientific.com>
2015-09-14 09:07:36 -0600

    Trying out multiple foldp's.

---
+++ Random.elm
@@ -0,0 +1,19 @@
+import Signal exposing (..)
+import Mouse
+import Graphics.Element exposing (..)
+import Graphics.Collage exposing (..)
+import Window
+import Time exposing (Time, fps, inMilliseconds)
+
+main = map show o
+
+delay = fps 30
+
+-- Trying out multiple foldp's
+totalTime = foldp (\dt s -> s + dt) 0 delay
+clicks = foldp (\x s -> s + 1) 0 Mouse.clicks
+
+o = (,,) <~ totalTime ~ Mouse.position ~ clicks
+
+
+signals = merge 
