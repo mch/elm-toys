a02bf639faac9495535a17c8debb65a4adc867c1
Matt Hughes <matt.hughes@calgaryscientific.com>
2016-03-31 22:59:19 -0600

    Used elm-format

--- ForWil.elm
+++ ForWil.elm
@@ -3,6 +3,9 @@
 --
 -- Except for the animation. That's all me.
 
+
+module Main (..) where
+
 import Graphics.Collage exposing (..)
 import Color exposing (black, white)
 import StartApp exposing (start)
@@ -10,59 +13,93 @@ import Time exposing (fps, Time)
 import Effects
 import Html exposing (fromElement)
 
-  
-xSize = 10
-pixelSize = 20
 
-picture = [ 0, 1, 0, 1, 0, 0, 1, 0, 0, 0
-          , 1, 0, 0, 1, 0, 0, 1, 0, 0, 0
-          , 0, 0, 1, 1, 1, 1, 1, 1, 0, 0
-          , 0, 1, 0, 1, 0, 1, 0, 1, 0, 1
-          , 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 
-          ]
+xSize =
+  10
+
+
+pixelSize =
+  20
+
+
+picture =
+  [ 0, 1, 0, 1, 0, 0, 1, 0, 0, 0
+  , 1, 0, 0, 1, 0, 0, 1, 0, 0, 0
+  , 0, 0, 1, 1, 1, 1, 1, 1, 0, 0
+  , 0, 1, 0, 1, 0, 1, 0, 1, 0, 1
+  , 0, 1, 0, 1, 0, 1, 0, 1, 0, 1
+  ]
+
 
 toBox : Int -> Int -> Form
 toBox index i =
-  let 
-    box = rect pixelSize pixelSize
-    xoffset = pixelSize * (index % 10)
-    yoffset = -pixelSize * (index // 10)
-    
-    color = 
+  let
+    box =
+      rect pixelSize pixelSize
+
+    xoffset =
+      pixelSize * (index % 10)
+
+    yoffset =
+      -pixelSize * (index // 10)
+
+    color =
       if i == 0 then
-        black 
-      else 
+        black
+      else
         white
-    
-    form = filled color box
+
+    form =
+      filled color box
   in
-    move (toFloat xoffset, toFloat yoffset) form
+    move ( toFloat xoffset, toFloat yoffset ) form
+
 
 rotate : List a -> List a
-rotate l = 
+rotate l =
   let
-    last = List.drop (List.length l - 1) l
+    last =
+      List.drop (List.length l - 1) l
   in
     List.take (List.length l) (List.append last l)
 
 
 
-
-
 -- Boiler plate...
-type alias Model = { picture : List Int }
-type Action = Tick Time
-init = ({ picture = picture }, Effects.none)
+
+
+type alias Model =
+  { picture : List Int }
+
+
+type Action
+  = Tick Time
+
+
+init =
+  ( { picture = picture }, Effects.none )
+
+
 update action m =
   ( { m | picture = rotate m.picture }, Effects.none )
+
+
 view addr m =
-  let 
-    forms = List.indexedMap toBox m.picture
+  let
+    forms =
+      List.indexedMap toBox m.picture
   in
     collage 800 600 forms |> fromElement
-  
-app = start { init = init, update = update, view = view, inputs = [(fps 1)] }
-main = app.html
+
+
+app =
+  start { init = init, update = update, view = view, inputs = [ (fps 1) ] }
+
+
+main =
+  app.html
+
 
 port runner : Signal (Task Effects.Never ())
-port runner = app.tasks
+port runner =
+  app.tasks
